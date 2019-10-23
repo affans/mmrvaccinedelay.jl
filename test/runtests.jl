5a927754ef23b2da03cfb4ea33b1a3d0bdb98311
affan s <affan.s@outlook.com>
2019-10-23 16:32:54 -0400

    modified and updated tests file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,57 +5,60 @@ const mmr = mmrvaccinedelay
 const gridsize = length(mmr.humans)
 const gridtime = 50*50
 const vax_coverage = 0.8
+
+# to do:
+# test whether group changes when individual is at the end of their age bracket
 @testset "INIT" begin
     mmr.init_population()
-    x = mmr.humans[1]
-    @test x.idx == 1
-    @test x.vaccinetime == 9999
-    @test x.vaccinated == false
     @test length(findall(x -> x == undef, mmr.humans)) == 0 ## check for proper initialization
-    for x in mmr.humans
-        @test x.ageofdeath == 4250
+    for i = 1:gridsize
+        x = mmr.humans[1]
+        @test x.idx == 1
+        @test x.vaccinetime == 9999
+        @test x.vaccinated == false
         @test x.swap == mmr.UNDEF
         @test x.health == mmr.SUSC
+        @test x.protection == 0
+        @test x.age <= x.ageofdeath
+        @test x.protection == 0 ## no one is vaccinated or protected. 
     end
     
-    ## test if the init_infected function really works.
+    ## test if the insert_infected function really works.
     mmr.init_population()
-    mmr.init_infected(1)
+    mmr.insert_infected(1)
     a = findall(x -> x.health == mmr.INF, mmr.humans)
     @test length(a) == 1
 
-    ## test if the init_infected dosn't sample the sample person twice.
+    ## test if the insert_infected dosn't sample the sample person twice.
     mmr.init_population()
-    mmr.init_infected(100)
+    mmr.insert_infected(100)
     a = findall(x -> x.health == mmr.INF, mmr.humans)
     @test length(a) == 100
 
-
+    ## test init_herdimmunity function
     mmr.init_population()
-    for x in mmr.humans 
-        @test x.protection == 0.0
-        @test x.health == mmr.SUSC
-    end
-    mmr.init_herdimmunity()
-    a1 = findall(x -> x.health == mmr.SUSC && x.age < 26, mmr.humans)
-    a2 = findall(x -> x.health == mmr.SUSC && x.age >= 26 && x.age < 50, mmr.humans)
+    mmr.init_herdimmunity(0)    
     a3 = findall(x -> x.health == mmr.REC, mmr.humans)
-    
-    for i in a1 
-        @test mmr.humans[i].protection == 1.0
-        @test mmr.humans[i].vaccinated == false ## extra test
+    @test length(a3) == 0 
+
+    hicov = 0.8
+    mmr.init_population()
+    hi_eligble = findall(x -> x.age >= 200 && x.health == mmr.SUSC, mmr.humans)
+    mmr.init_herdimmunity(hicov)    
+    a3 = findall(x -> x.health == mmr.REC, mmr.humans)
+    @test isapprox(length(a3)/length(hi_eligble), hicov; atol=0.1)
+
+    # test if susceptibles are still with 0 protection
+    for x in mmr.humans 
+        @test x.health == mmr.SUSC || x.health == mmr.REC ## should not get an infected
+        @test x.vaccinated == false 
+        @test x.vaccinetime == 9999
+        if x.health == mmr.SUSC 
+            @test x.protection == 0
+        elseif x.health == mmr.REC
+            @test x.protection == 1
+        end
     end
-    for i in a2
-        @test mmr.humans[i].protection == 0.5
-        @test mmr.humans[i].vaccinated == false ## extra test
-    end
-    for i in a3
-        @test mmr.humans[i].protection == 1.0
-        @test mmr.humans[i].vaccinated == false ## extra test
-    end
-    p = findall(x -> x.age >= 200, mmr.humans)
-    @test isapprox(length(a3)/length(p), 0.80; atol=0.1)
-    
 end
 
 @testset "AGE" begin
@@ -64,58 +67,20 @@ end
     ## checks if age is incremented properly
     ## and verifies dead people are replaced properly. 
     a1 = [mmr.humans[i].age for i = 1:gridsize]
-    ls = zeros(Bool, gridsize) #contains whether they have left the system
+    leave = zeros(Bool, gridsize) #contains whether they have left the system
     for i = 1:gridsize
-        r = mmr.age_and_vaccinate(mmr.humans[i])
-        ls[i] = r[2] # r[2] contains whether they have left the system
+        ls, li = mmr.age_and_death(mmr.humans[i])
+        leave[i] = ls 
     end
+    ## check if the age_and_death worked properly. 
     a2 = [mmr.humans[i].age for i = 1:gridsize]
     for i = 1:gridsize
         @test (a2[i] == a1[i] + 1) || a2[i] == 0
     end
+    # test whether the number of newborns = number of people left
     l1 = length(findall(x -> x == 0, a2))
-    l2 = length(findall(x -> x == true, ls))
+    l2 = length(findall(x -> x == true, leave))
     @test l1 == l2
-
-    ## check whether group changes (may as well loop through all humans, all groups)
-    mmr.init_population()
-    for x in mmr.humans
-    #x = mmr.humans[1]
-    for grp = 1:15 
-        # set their age to the end of the group and see if age and group changes accordingly
-        x.age = mmr.agebraks[grp][end]
-        x.group = mmr.get_group(x.age) 
-        
-        @test x.group == grp
-        @test x.ageofdeath == 4250 ## see if this dosn't change
-        oldage = x.age
-        oldgrp = x.group
-        r = mmr.age_and_vaccinate(x)
-        @test r[1] == true  ## test if the age_and_vaccinate returns true for group change
-     
-        if grp < 15  ## if group is less than 15 .. they won't be reset
-            @test x.age == oldage + 1
-            @test x.group == oldgrp + 1
-            @test x.group == mmr.get_group(x.age) ## this tests the function
-        else # since group was 15 and their age was set to the last age
-            @test x.age == 0
-            @test x.group == 1
-            @test x.group == mmr.get_group(x.age)
-            @test r[2] == true ## r[2] holds the true/value whether person has left.
-        end
-
-        # set their age to the START of the group and see if age and group changes accordingly 
-        # group should not change, no one should die here either
-        x.age = mmr.agebraks[grp][1]
-        x.group = mmr.get_group(x.age) 
-        oldage = x.age
-        r = mmr.age_and_vaccinate(x)
-        @test r[1] == false
-        @test r[2] == false
-        @test x.group == grp 
-        @test (x.age == oldage + 1)
-    end
-    end
 
     ## this is not a test. basically runs the model for gridtime and 
     ## stores who left the system. this needs to be plotted or something
@@ -123,87 +88,16 @@ end
     wd = zeros(Int64, gridtime)
     for t = 1:gridtime
         for x in mmr.humans 
-            r = mmr.age_and_vaccinate(x)
+            r = mmr.age_and_death(x)
             if r[2] == true 
                 wd[t] += 1
             end
         end
     end
     #UnicodePlots.scatterplot(wd)
-
-    ## we test that if everywhere was born then within 50 years they should not leave the system
-    ## secondary test that ID of the person should not be changing. the .idx proeprty of human is important
-    mmr.init_population()
-    wd = zeros(Int64, gridtime)
-    for x in mmr.humans 
-        x.age = 0 
-        x.group = 1
-    end
-    for t = 1:gridtime
-        for x in mmr.humans 
-            oldid = x.idx
-            r = mmr.age_and_vaccinate(x)
-            if r[2] == true
-                wd[t] += 1
-            end
-        end
-    end
-    @test sum(wd) == 0
-
-   
-    # # test agm vector. this is used in the contact patterns. 
-    # mmr.init_population()
-    # x = mmr.humans[1]
-    # x.age = 0
-    # x.group = 1
-    # _agm = Vector{Vector{Int64}}(undef, 15)
-    # mmr.setup_agm(_agm)
-    # @test 1 ∈ _agm[1] ## test the manual human above. 
-    # for x in mmr.humans  # test everyone
-    #     @test x.idx ∈ _agm[x.group]         
-    # end
-    # x.age = 201
-    # x.group = mmr.get_group(x.age)
-    # mmr.setup_agm(_agm)
-    # @test 1 ∉ _agm[1] # human changed group so should not in the first bin
-    # @test 1 ∈ _agm[2]
-
-    # ## agm
-    # mmr.init_population()
-    # _agm = Vector{Vector{Int64}}(undef, 15)
-    # mmr.setup_agm(_agm) ## fill this array
-    # @test sum(length.(_agm))  == gridsize
-    # for i = 1:10 ## test this 5000 times to make sure the sum remains the same...
-    #     for x in mmr.humans
-    #         r = mmr.age_and_vaccinate(x)
-    #     end
-    #     @test sum(length.(_agm))  == gridsize
-    # end    
-
-    # ## agm should only contain susceptibles. 
-    # mmr.init_population()
-    # _agm = Vector{Vector{Int64}}(undef, 15)
-    # mmr.setup_agm(_agm) ## fill this array
-    # for ag in _agm
-    #     for g in ag
-    #         @test mmr.humans[g].health == mmr.SUSC
-    #     end
-    # end
-
-    # ## test after aging... 
-    # mmr.init_population()
-    # _agm = Vector{Vector{Int64}}(undef, 15)
-    # mmr.setup_agm(_agm) ## fill this array
-    # for ag in _agm
-    #     for g in ag
-    #         @test mmr.humans[g].health == mmr.SUSC
-    #     end
-    # end
-
 end
 
 @testset "SWAPS" begin
-
     ## This tests whether the person's swaps are set properly.
     ## secondary tests makes sure if person isn't infected, they can't transmit the disease even with beta = 1
     mmr.init_population()
@@ -213,48 +107,67 @@ end
     mmr.update_swaps()           # update swap
     @test x.swap == mmr.UNDEF    # tests whether swap happened
     @test x.health == mmr.INF
-    # prep for the loop
-    _agm = Vector{Vector{Int64}}(undef, 15)
-    mmr.setup_agm(_agm)
+ 
     # go through one week and see what happens after 
+    agm = Vector{Vector{Int64}}(undef, 15) # array of array, holds stratified population
+    for grp = 1:15
+        agm[grp] = findall(x -> x.health == mmr.SUSC && x.group == grp, mmr.humans)    
+    end 
     for i in 1:gridsize
-        cntinf = mmr.contact_dynamic2(mmr.humans[i], 1.0, _agm)
-        # we only made the first human infected, so the number of secondary infections should be zero for everyone else
-        # @test cntinf > 0 ## this could fail.... if number of contacts is zero      
+        d, c = mmr.contact_dynamic2(mmr.humans[i], 1.0, agm)
+        # we only made the first human infected, so the number of secondary infections should be zero for everyone else        
     end 
     @test x.health == mmr.INF    # human[1] is still infected 
     mmr.update_swaps()           # this should move their health to REC
-    @test x.health == mmr.REC    # test whether it does
+    @test x.health == mmr.REC    # test whether it recovers back to REC (it will fail if INC -> SUSC depending on vaccine/prevaccine era)
     @test x.swap == mmr.UNDEF
     # test the population to see only one recovered
     @test 1 == length(findall(x -> x.health == mmr.REC, mmr.humans))
     @test length(findall(x -> x.health == mmr.INF, mmr.humans)) >= 0 
-
-    ## to do, run the swap function with infweek set to 2. 
 end
 
 @testset "VAX" begin
-    ## test that all vaccine time is above 52 weeks
+    ## test vaccination coverage and protection level
     mmr.init_population()
-    mmr.init_vaccination()
+    mmr.init_vaccination(vax_coverage)
     v = 0
     for x in mmr.humans
+        mmr.apply_protection(x) ## apply protection if vaccinated
+        
+
         if x.vaccinated 
-            @test x.age >= 50 && x.age < 200
-            @test x.health == mmr.SUSC
+            @test x.age >= 50 && x.age < 200            
+            @test x.protection == 0.95
             v += 1
+        else
+            @test x.protection == 0.0 
         end
     end
     a1 = length(findall(x -> x.age >= 50 && x.age < 200, mmr.humans))
-    @test isapprox(v/a1, vax_coverage; atol=0.1)
+    #@test isapprox(v/a1, vax_coverage; atol=0.1)
 
-    # we test the apply_protection at different stages.
-    # check for herd_immunity. 
-    mmr.init_population()
-    mmr.init_herdimmunity() 
+    ## check if a newborn kills the protection level 
     for x in mmr.humans
-        if x.health == mmr.REC 
-            @test x.protection == 1.0
+        if x.vaccinated == true 
+            @test x.protection == 0.95
+        end
+        mmr.newborn(x)
+        @test x.protection == 0.0
+        @test x.vaccinated == false
+        @test x.vaccinetime == 9999
+    end
+
+    ## check whether vaccinated, they always stay vaccinated. 
+    mmr.init_population()
+    mmr.init_vaccination(vax_coverage)
+    for t = 1:gridtime
+        for x in mmr.humans 
+            r = mmr.age_and_death(x)
+            vc = mmr.vaccinate(x)
+            mmr.apply_protection(x)
+            if x.vaccinated 
+                @test x.protection = 0.95
+            end            
         end
     end
 end
